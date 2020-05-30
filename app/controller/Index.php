d8091e63efa06785b749dec1fb46c64a19f22394
walkor <walkor@workerman.net>
2020-05-30 15:09:46 +0800

    not install database and redis by default

--- app/controller/Index.php
+++ app/controller/Index.php
@@ -2,32 +2,23 @@
 namespace app\controller;
 
 use support\Request;
+use JasonGrimes\Paginator;
+use Symfony\Component\Translation\Translator;
+use Symfony\Component\Translation\Formatter\IntlFormatter;
 
-class Index
+class User
 {
-    public function index(Request $request)
+    /**
+     * 用户列表
+     */
+    public function get(Request $request)
     {
-        return response('hello webman');
+        return 'ok';
+        locale('fr');
+        return  response(trans(
+            'apple',
+            ['%count%' => 1]
+        ));
     }
 
-    public function view(Request $request)
-    {
-        return view('index/view', ['name' => 'webman']);
-    }
-
-    public function json(Request $request)
-    {
-        return json(['code' => 0, 'msg' => 'ok']);
-    }
-
-    public function file(Request $request)
-    {
-        $file = $request->file('upload');
-        if ($file && $file->isValid()) {
-            $file->move(public_path().'/files/myfile.'.$file->getExtension());
-            return json(['code' => 0, 'msg' => 'upload success']);
-        }
-        return json(['code' => 1, 'msg' => 'file not found']);
-    }
-    
 }
