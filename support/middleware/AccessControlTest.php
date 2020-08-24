ae4223857ca9709446763d93f83b40598ed5ad8e
walkor <walkor@users.noreply.github.com>
2020-08-24 14:35:02 +0800

    Update AccessControlTest.php

--- support/middleware/AccessControlTest.php
+++ support/middleware/AccessControlTest.php
@@ -22,14 +22,21 @@ class AccessControlTest implements MiddlewareInterface
 {
     public function process(Request $request, callable $next) : Response
     {
-        /** @var Response $response */
-        $response = $next($request);
         // 允许uri以 /api 开头的地址跨域访问
         if (strpos($request->path(), '/api') === 0) {
+            // 如果是options请求，不处理业务
+            if ($request->method() == 'OPTIONS') {
+                $response = response('');
+            } else {
+                $response = $next($request);
+            }
             $response->withHeaders([
-                'Access-Control-Allow-Origin'      => '*',
-                'Access-Control-Allow-Credentials' => 'true',
+                'Access-Control-Allow-Origin' => '*',
+                'Access-Control-Allow-Methods' => 'GET,POST,PUT,DELETE,OPTIONS',
+                'Access-Control-Allow-Headers' => 'Content-Type,Authorization,X-Requested-With,Accept,Origin',
             ]);
+        } else {
+            $response = $next($request);
         }
         return $response;
     }
