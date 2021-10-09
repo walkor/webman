dc8ec436bcb010a05e566e86a2b271cbb9f798b1
walkor <walkor@users.noreply.github.com>
2021-10-09 20:52:22 +0800

    Update helpers.php

--- support/helpers.php
+++ support/helpers.php
@@ -281,6 +281,10 @@ function worker_bind($worker, $class) {
  * @return int
  */
 function cpu_count() {
+    // Windows does not support the number of processes setting.
+    if (\DIRECTORY_SEPARATOR === '\\') {
+        return 1;
+    }
     if (strtolower(PHP_OS) === 'darwin') {
         $count = shell_exec('sysctl -n machdep.cpu.core_count');
     } else {
