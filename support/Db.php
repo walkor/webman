d0e5f3096822895c2171a3db930dbb87c2f59cac
maoxp <maoxingpei8686@163.com>
2021-11-01 10:11:15 +0800

    fixed: 补充静态方法注解

--- support/Db.php
+++ support/Db.php
@@ -14,19 +14,25 @@
 namespace support;
 
 use Illuminate\Database\Capsule\Manager;
+use Illuminate\Database\Connection;
 
 /**
  * Class Db
  * @package support
  * @method static array select(string $query, $bindings = [], $useReadPdo = true)
- * @method static int insert(string $query, $bindings = [])
+ * @method static mixed selectOne(string $query, $bindings = [], $useReadPdo = true)
+ * @method static array selectFromWriteConnection(string $query, $bindings = [], $useReadPdo = true)
+ * @method static bool unprepared(string $query)
+ * @method static bool insert(string $query, $bindings = [])
+ * @method static bool statement(string $query, $bindings = [])
  * @method static int update(string $query, $bindings = [])
  * @method static int delete(string $query, $bindings = [])
- * @method static bool statement(string $query, $bindings = [])
+ * @method static int affectingStatement(string $query, $bindings = [])
  * @method static mixed transaction(\Closure $callback, $attempts = 1)
  * @method static void beginTransaction()
  * @method static void rollBack($toLevel = null)
  * @method static void commit()
+ * @method static Connection beforeExecuting(\Closure $callback)
  */
 class Db extends Manager
 {
