47a17b2e467cfc486cc85543eab28c4160609b0a
maoxp <maoxingpei8686@163.com>
2021-11-01 08:35:21 +0800

    Db static method

--- support/Db.php
+++ support/Db.php
@@ -18,6 +18,15 @@ use Illuminate\Database\Capsule\Manager;
 /**
  * Class Db
  * @package support
+ * @method static array select(string $query, $bindings = [], $useReadPdo = true)
+ * @method static int insert(string $query, $bindings = [])
+ * @method static int update(string $query, $bindings = [])
+ * @method static int delete(string $query, $bindings = [])
+ * @method static bool statement(string $query, $bindings = [])
+ * @method static mixed transaction(\Closure $callback, $attempts = 1)
+ * @method static void beginTransaction()
+ * @method static void rollBack($toLevel = null)
+ * @method static void commit()
  */
 class Db extends Manager
 {
