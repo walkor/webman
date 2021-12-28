2d3fdf85e317cb151201e59083b9095c3c65bd8f
maoxp <maoxingpei8686@163.com>
2021-12-28 15:46:15 +0800

    fixed .env format

--- app/controller/Index.php
+++ app/controller/Index.php
@@ -1,12 +1,14 @@
 <?php
 namespace app\controller;
 
+use support\Env;
 use support\Request;
 
 class Index
 {
     public function index(Request $request)
     {
+        var_dump(Env::get());
         return response('hello webman');
     }
 
