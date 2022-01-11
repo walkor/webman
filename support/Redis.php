cb3b14ff81b603eeb1429143c058406e92567a23
walkor <walkor@users.noreply.github.com>
2022-01-11 19:18:22 +0800

    fix #273

--- support/Redis.php
+++ support/Redis.php
@@ -178,7 +178,7 @@ use Illuminate\Redis\RedisManager;
  * @method static mixed watch($keys)
  * @method static mixed unwatch($keys)
  * Scripting methods
- * @method static mixed eval($script, $args = [], $numKeys = 0)
+ * @method mixed eval($script, $numkeys, $keyOrArg1 = null, $keyOrArgN = null)
  * @method static mixed evalSha($sha, $args = [], $numKeys = 0)
  * @method static mixed script($command, ...$scripts)
  * @method static mixed client(...$args)
