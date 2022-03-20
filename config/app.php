64e2455df269b6f8fd279038fb59044115ad4192
walkor <walkor@users.noreply.github.com>
2022-03-20 20:56:23 +0800

    app.request_class

--- config/app.php
+++ config/app.php
@@ -12,9 +12,12 @@
  * @license   http://www.opensource.org/licenses/mit-license.php MIT License
  */
 
+use support\Request;
+
 return [
     'debug' => true,
     'default_timezone' => 'Asia/Shanghai',
+    'request_class' => Request::class,
     'public_path' => base_path() . DIRECTORY_SEPARATOR . 'public',
     'runtime_path' => base_path(false) . DIRECTORY_SEPARATOR . 'runtime',
     'controller_suffix' => '',
