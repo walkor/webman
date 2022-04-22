198446c9671da730bae4599231f6ac5c014aba50
walkor <walkor@users.noreply.github.com>
2022-04-22 15:17:08 +0800

    Update start.php

--- start.php
+++ start.php
@@ -67,7 +67,7 @@ if (property_exists(Worker::class, 'statusFile')) {
     Worker::$statusFile = $config['status_file'] ?? '';
 }
 if (property_exists(Worker::class, 'stopTimeout')) {
-    Worker::$stopTimeout = $config['stop_timeout'] ?? '';
+    Worker::$stopTimeout = $config['stop_timeout'] ?? 2;
 }
 
 if ($config['listen']) {
