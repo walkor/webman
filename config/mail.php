2acf2db4c09361c9447d2807cd18dc1b3727e28e
Amir <faramarzii.amir@gmail.com>
2022-07-25 12:22:46 +0430

    add config/mail.php

---
+++ config/mail.php
@@ -0,0 +1,15 @@
+<?php
+
+/*
+ * driver => smtp(default),mail,sendmail,qmail
+ */
+return [
+    'driver' => 'smtp' ,
+    'encryption' => null ,
+    'fromAddress' => 'hello@example.com' ,
+    'fromName' => 'Example' ,
+    'host' => 'smtp.mailtrap.io' ,
+    'username' => null ,
+    'password' => null ,
+    'port' => 2525
+];
