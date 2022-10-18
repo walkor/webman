0aa8953414a058222c34ba09bf31f1be8e16c49f
mouyong <mouyong@users.noreply.github.com>
2022-10-18 15:24:50 +0800

    Update start.php

--- start.php
+++ start.php
@@ -1,4 +1,9 @@
 #!/usr/bin/env php
 <?php
+
+// 避免加载了 laravel/illuminate/foundation/helper.php 导致无法控制顺序的函数重定义报错
+require_once __DIR__ . '/support/helpers.php';
+
 require_once __DIR__ . '/vendor/autoload.php';
+
 support\App::run();
