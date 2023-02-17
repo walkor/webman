dc7d100690960c2198d294d80551199a8d116c0e
walkor <walkor@users.noreply.github.com>
2023-02-17 11:20:40 +0800

    Update bootstrap.php

--- support/bootstrap.php
+++ support/bootstrap.php
@@ -16,29 +16,29 @@ use Dotenv\Dotenv;
 use support\Log;
 use Webman\Bootstrap;
 use Webman\Config;
-use Webman\Route;
 use Webman\Middleware;
+use Webman\Route;
 use Webman\Util;
 
 $worker = $worker ?? null;
 
-set_error_handler(static function ($level, $message, $file = '', $line = 0) {
+set_error_handler(function ($level, $message, $file = '', $line = 0) {
     if (error_reporting() & $level) {
         throw new ErrorException($message, 0, $level, $file, $line);
     }
 });
 
 if ($worker) {
-    register_shutdown_function(static function ($startTime) {
-        if (time() - $startTime <= 1) {
+    register_shutdown_function(function ($startTime) {
+        if (time() - $startTime <= 0.1) {
             sleep(1);
         }
     }, time());
 }
 
 if (class_exists('Dotenv\Dotenv') && file_exists(base_path() . '/.env')) {
-    if (method_exists('Dotenv\Dotenv', 'createUnsafeImmutable')) {
-        Dotenv::createUnsafeImmutable(base_path())->load();
+    if (method_exists('Dotenv\Dotenv', 'createUnsafeMutable')) {
+        Dotenv::createUnsafeMutable(base_path())->load();
     } else {
         Dotenv::createMutable(base_path())->load();
     }
@@ -67,20 +67,20 @@ foreach (config('plugin', []) as $firm => $projects) {
     }
 }
 
-Middleware::load(config('middleware', []), '');
+Middleware::load(config('middleware', []));
 foreach (config('plugin', []) as $firm => $projects) {
     foreach ($projects as $name => $project) {
         if (!is_array($project) || $name === 'static') {
             continue;
         }
-        Middleware::load($project['middleware'] ?? [], '');
+        Middleware::load($project['middleware'] ?? []);
     }
     Middleware::load($projects['middleware'] ?? [], $firm);
     if ($staticMiddlewares = config("plugin.$firm.static.middleware")) {
         Middleware::load(['__static__' => $staticMiddlewares], $firm);
     }
 }
-Middleware::load(['__static__' => config('static.middleware', [])], '');
+Middleware::load(['__static__' => config('static.middleware', [])]);
 
 foreach (config('bootstrap', []) as $className) {
     if (!class_exists($className)) {
@@ -110,6 +110,7 @@ foreach (config('plugin', []) as $firm => $projects) {
         }
     }
     foreach ($projects['bootstrap'] ?? [] as $className) {
+        /** @var string $className */
         if (!class_exists($className)) {
             $log = "Warning: Class $className setting in plugin/$firm/config/bootstrap.php not found\r\n";
             echo $log;
@@ -129,4 +130,3 @@ foreach (Util::scanDir($directory) as $path) {
     }
 }
 Route::load($paths);
-
