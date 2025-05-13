46829bb85952a956e77578beb112b16f4f07f6e9
liming <1628883533@qq.com>
2025-05-13 15:39:37 +0800

    修改中间件参数next为handler

--- app/middleware/StaticFile.php
+++ app/middleware/StaticFile.php
@@ -24,14 +24,14 @@ use Webman\Http\Request;
  */
 class StaticFile implements MiddlewareInterface
 {
-    public function process(Request $request, callable $next): Response
+    public function process(Request $request, callable $handler): Response
     {
         // Access to files beginning with. Is prohibited
         if (strpos($request->path(), '/.') !== false) {
             return response('<h1>403 forbidden</h1>', 403);
         }
         /** @var Response $response */
-        $response = $next($request);
+        $response = $handler($request);
         // Add cross domain HTTP header
         /*$response->withHeaders([
             'Access-Control-Allow-Origin'      => '*',
